dc63f2a709f1c0c86da066d739deb9f53e4e821e
Kisleto <zsviktor45@gmail.com>
2019-03-24 14:02:44 +0100

    refactor of comicservice

--- comics-service/src/main/java/com/codecool/comics/ComicService.java
+++ comics-service/src/main/java/com/codecool/comics/ComicService.java
@@ -7,20 +7,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
-import java.io.Serializable;
-import java.net.MalformedURLException;
-import java.util.HashMap;
 import java.util.Random;
 
 @Service
 @RestController
 @Data
-public class ComicService implements Serializable {
+public class ComicService {
 
 
     @Autowired
